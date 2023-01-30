4c72824e9230d56c09815d36326ec166dc5acb1c
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2023-01-30 18:51:44 +0600

    Handled Form By useReducer

--- src/pages/LongForm.js
+++ src/pages/LongForm.js
@@ -1,191 +1,245 @@
-import React, { useReducer } from "react";
-import { initialState, reducer } from "../state/formReducer";
+import React from "react";
+import { useReducer } from "react";
 
 const LongForm = () => {
+  const initialState = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    gender: "",
+    education: "",
+    quantity: 0,
+    feedback: "",
+    term: false,
+  };
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "INPUT":
+        return {
+          ...state,
+          [action.payload.name]: action.payload.value,
+        };
+      case "INCREMENT":
+        return {
+          ...state,
+          quantity: state.quantity + action.payload.count,
+        };
+      case "DECREMENT":
+        return {
+          ...state,
+          quantity: state.quantity - action.payload.count,
+        };
+      case "TOGGLE":
+        return {
+          ...state,
+          term: !state.term,
+        };
+      default:
+        return state;
+    }
+  };
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const submit = (event) => {
-    event.preventDefault();
-
+  const submitForm = (e) => {
+    e.preventDefault();
     console.log(state);
   };
-
   return (
-    <div className='h-screen w-screen flex justify-center items-center overflow-auto'>
+    <div className="h-screen w-screen flex justify-center items-center overflow-auto">
       <form
-        className='shadow-lg p-10 rounded-md flex flex-wrap gap-3 max-w-3xl justify-between'
-        onSubmit={submit}
+        className="shadow-lg p-10 rounded-md flex flex-wrap gap-3 max-w-3xl justify-between"
+        onSubmit={submitForm}
       >
-        <div className='flex flex-col w-full max-w-xs'>
-          <label className='mb-2' htmlFor='firstName'>
+        <div className="flex flex-col w-full max-w-xs">
+          <label className="mb-2" htmlFor="firstName">
             First Name
           </label>
           <input
-            type='text'
-            name='firstName'
-            id='firstName'
-            onBlur={(e) =>
+            type="text"
+            name="firstName"
+            id="firstName"
+            onBlur={(e) => {
               dispatch({
                 type: "INPUT",
                 payload: { name: e.target.name, value: e.target.value },
-              })
-            }
+              });
+            }}
           />
         </div>
-        <div className='flex flex-col w-full max-w-xs'>
-          <label className='mb-2' htmlFor='lastName'>
+        <div className="flex flex-col w-full max-w-xs">
+          <label className="mb-2" htmlFor="lastName">
             Last Name
           </label>
           <input
-            type='text'
-            name='lastName'
-            id='lastName'
-            onBlur={(e) =>
+            type="text"
+            name="lastName"
+            id="lastName"
+            onBlur={(e) => {
               dispatch({
                 type: "INPUT",
                 payload: { name: e.target.name, value: e.target.value },
-              })
-            }
+              });
+            }}
           />
         </div>
-        <div className='flex flex-col w-full max-w-xs'>
-          <label className='mb-2' htmlFor='email'>
+        <div className="flex flex-col w-full max-w-xs">
+          <label className="mb-2" htmlFor="email">
             Email
           </label>
           <input
-            type='email'
-            name='email'
-            id='email'
-            onBlur={(e) =>
+            type="email"
+            name="email"
+            id="email"
+            onBlur={(e) => {
               dispatch({
                 type: "INPUT",
                 payload: { name: e.target.name, value: e.target.value },
-              })
-            }
+              });
+            }}
           />
         </div>
-        <div className='flex flex-col w-full max-w-xs'>
-          <h1 className='mb-3'>Gender</h1>
-          <div className='flex gap-3'>
+        <div className="flex flex-col w-full max-w-xs">
+          <h1 className="mb-3">Gender</h1>
+          <div className="flex gap-3">
             <div>
               <input
-                type='radio'
-                id='male'
-                name='gender'
-                value='male'
-                onClick={(e) =>
+                type="radio"
+                id="male"
+                name="gender"
+                value="male"
+                onClick={(e) => {
                   dispatch({
                     type: "INPUT",
                     payload: { name: e.target.name, value: e.target.value },
-                  })
-                }
+                  });
+                }}
               />
-              <label className='ml-2 text-lg' for='male'>
+              <label className="ml-2 text-lg" htmlFor="male">
                 Male
               </label>
             </div>
             <div>
               <input
-                type='radio'
-                id='female'
-                name='gender'
-                value='female'
-                onClick={(e) =>
+                type="radio"
+                id="female"
+                name="gender"
+                value="female"
+                onClick={(e) => {
                   dispatch({
                     type: "INPUT",
                     payload: { name: e.target.name, value: e.target.value },
-                  })
-                }
+                  });
+                }}
               />
-              <label className='ml-2 text-lg' for='female'>
+              <label className="ml-2 text-lg" htmlFor="female">
                 Female
               </label>
             </div>
             <div>
               <input
-                type='radio'
-                id='other'
-                name='gender'
-                value='other'
-                onClick={(e) =>
+                type="radio"
+                id="other"
+                name="gender"
+                value="other"
+                onClick={(e) => {
                   dispatch({
                     type: "INPUT",
                     payload: { name: e.target.name, value: e.target.value },
-                  })
-                }
+                  });
+                }}
               />
-              <label className='ml-2 text-lg' for='other'>
+              <label className="ml-2 text-lg" htmlFor="other">
                 Other
               </label>
             </div>
           </div>
         </div>
-        <div className='flex flex-col w-full max-w-xs'>
-          <label className='mb-3' for='education'>
+        <div className="flex flex-col w-full max-w-xs">
+          <label className="mb-3" htmlFor="education">
             Education
           </label>
           <select
-            name='education'
-            id='education'
-            onChange={(e) =>
+            name="education"
+            id="education"
+            onChange={(e) => {
               dispatch({
                 type: "INPUT",
                 payload: { name: e.target.name, value: e.target.value },
-              })
-            }
+              });
+            }}
           >
-            <option value='SSC'>SSC</option>
-            <option value='HSC'>HSC</option>
-            <option value='underGrad'>Under Graduate</option>
-            <option value='graduate'>Graduate</option>
+            <option value="SSC">SSC</option>
+            <option value="HSC">HSC</option>
+            <option value="underGrad">Under Graduate</option>
+            <option value="graduate">Graduate</option>
           </select>
         </div>
-        <div className='flex flex-col w-full max-w-xs'>
-          <label className='mb-3'>Number of PCs</label>
-          <div className='flex justify-between items-center gap-2 '>
-            <button className='bg-indigo-500 text-lg text-white rounded h-10 w-10 '>
+        <div className="flex flex-col w-full max-w-xs">
+          <label className="mb-3">Number of PCs</label>
+          <div className="flex justify-between items-center gap-2 ">
+            <button
+              className="bg-indigo-500 text-lg text-white rounded h-10 w-10 "
+              onClick={(e) => {
+                dispatch({
+                  type: "DECREMENT",
+                  payload: { count: 1 },
+                });
+              }}
+            >
               -
             </button>
-            <div className='border flex-1 flex justify-center items-center h-10 rounded-md border-gray-300'>
-              <span className='text-lg'>0</span>
+            <div className="border flex-1 flex justify-center items-center h-10 rounded-md border-gray-300">
+              <span className="text-lg">{state.quantity}</span>
             </div>
-            <button className='bg-indigo-500 text-lg text-white rounded h-10 w-10'>
+            <button
+              className="bg-indigo-500 text-lg text-white rounded h-10 w-10"
+              onClick={(e) => {
+                dispatch({
+                  type: "INCREMENT",
+                  payload: { count: 1 },
+                });
+              }}
+            >
               +
             </button>
           </div>
         </div>
-        <div className='flex flex-col w-full max-w-xs'>
-          <label className='mb-3' for='feedback'>
+        <div className="flex flex-col w-full max-w-xs">
+          <label className="mb-3" htmlFor="feedback">
             Feedback
           </label>
           <textarea
-            name='feedback'
-            id='feedback'
-            cols='30'
-            rows='4'
-            onBlur={(e) =>
+            name="feedback"
+            id="feedback"
+            cols="30"
+            rows="4"
+            onBlur={(e) => {
               dispatch({
                 type: "INPUT",
                 payload: { name: e.target.name, value: e.target.value },
-              })
-            }
+              });
+            }}
           ></textarea>
         </div>
 
-        <div className='flex justify-between items-center w-full'>
-          <div className='flex  w-full max-w-xs'>
+        <div className="flex justify-between items-center w-full">
+          <div className="flex  w-full max-w-xs">
             <input
-              className='mr-3'
-              type='checkbox'
-              name='term'
-              id='terms'
-              onClick={() => dispatch({ type: "TOGGLE" })}
+              className="mr-3"
+              type="checkbox"
+              name="term"
+              id="terms"
+              onClick={(e) => {
+                dispatch({
+                  type: "TOGGLE",
+                });
+              }}
             />
-            <label for='terms'>I agree to terms and conditions</label>
+            <label htmlFor="terms">I agree to terms and conditions</label>
           </div>
           <button
-            className=' px-4 py-3 bg-indigo-500 rounded-md font-semibold text-white text-lg disabled:bg-gray-500'
-            type='submit'
+            className=" px-4 py-3 bg-indigo-500 rounded-md font-semibold text-white text-lg disabled:bg-gray-500"
+            type="submit"
             disabled={!state.term}
           >
             Submit
